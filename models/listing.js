bc64f2ee6d78cdb62dfa3ab5f3411de12aa89ab9
varunn_ranaa <varunrana1777@gmail.com>
2026-02-04 14:22:08 +0530

    schemas

---
+++ models/listing.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const listSchema = new mongoose.Schema({
+   title : {
+    type : String,
+    required : true
+   },
+   description : {
+    type : String,
+    required : true
+   },
+   img : {
+    type : String,
+    set : (v) => v===""?"default Link": v,   //using setter function
+   },
+   price :{
+    type : Number,
+    required : true
+   },
+   location : {
+    type : String,
+    required : true
+   },
+   country : {
+    type : String,
+    required : true
+   }
+
+})
+
+const Listing = mongoose.model("Listing",listSchema);
+module.exports = Listing ;
