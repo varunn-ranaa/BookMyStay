e59d5fd281eea004dd06e265ce844e326a66f9d4
varunn_ranaa <varunrana1777@gmail.com>
2026-02-04 14:20:51 +0530

    Initialize DB

---
+++ init/index.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Listing = require('../models/listing.js');
+const initData = require('./data.js');
+
+connect()
+.then(res => console.log("Connection Sucessfull !"))
+.catch(err => console.log("Connection Failed !"));
+
+
+async function connect(){
+  await mongoose.connect('mongodb://127.0.0.1:27017/BookStay')
+}
+
+const initDB = async function (){
+    await Listing.deleteMany({});
+    await Listing.insertMany(initData.data);
+}
+
+initDB();
