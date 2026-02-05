cdbc88217462df5d809c31644defeff402214388
varunn_ranaa <varunrana1777@gmail.com>
2026-02-05 15:44:18 +0530

    Data Input to DB

--- init/index.js
+++ init/index.js
@@ -11,7 +11,7 @@ async function connect(){
   await mongoose.connect('mongodb://127.0.0.1:27017/BookStay')
 }
 
-const initDB = async function (){
+const initDB = async function (){s
     await Listing.deleteMany({});
     await Listing.insertMany(initData.data);
     console.log("Data initallized");
