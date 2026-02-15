b861ecd1b7e64c483b2cb1758a3490cd99236d9a
varunn_ranaa <varunrana1777@gmail.com>
2026-02-15 10:23:05 +0530

    backend Validation

---
+++ schemaValidation.js
@@ -0,0 +1,14 @@
+const Joi = require('joi');
+
+const listingSchema = Joi.object({
+    listing : Joi.object({
+        title : Joi.string().required().min(6).max(30),
+        description : Joi.string().required().min(15).max(100),
+        price : Joi.number().min(0).required(),
+        country : Joi.string().required(),
+        location : Joi.string().required(),
+        image : Joi.string().allow('',null)
+    }).required()
+})
+
+module.exports = {listingSchema};
