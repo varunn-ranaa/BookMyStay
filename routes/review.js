a6617976a78c4a0da39595c04ab0fd65199f1434
varunn_ranaa <varunrana1777@gmail.com>
2026-03-13 17:53:04 +0530

    review route

---
+++ routes/review.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const router = express.Router({mergeParams : true});
+const wrapAsync = require('../utils/wrapAsync.js');
+const ExpressError = require('../utils/expressError.js');
+const Listing = require('../models/listing.js');
+const Review = require('../models/review.js')
+const {revSchema} =  require('../schemaValidation.js');
+
+//reviews validation
+function reviewValidation(req,res,next){
+   let {value, error} = revSchema.validate(req.body);
+   if(error){
+    console.log(error);
+     let errMsg = error.details.map(el => el.message).join(",");
+     throw new ExpressError(400,errMsg);
+   }
+   else{
+    next();
+   }
+}
+
+//:Post route
+router.post("/",reviewValidation,wrapAsync(async(req,res)=>{
+   let {id} = req.params;
+   let data = req.body;
+   let rev = await new Review(data.review);
+   let newReview = await rev.save();
+    
+   let list = await Listing.findById(id);
+   list.review.push(newReview);
+   list.save();
+   console.log("New Review saved !");
+   res.redirect(`/listing/${id}`);
+  })
+);
+
+//:Delete review route
+router.delete("/:rid",wrapAsync(async(req,res)=>{
+  let {id,rid} = req.params;
+  await Listing.findByIdAndUpdate(id,{$pull : {review : rid}});
+  await Review.findByIdAndDelete(rid);
+  res.redirect(`/listing/${id}`);
+}));
+
+module.exports = router;
