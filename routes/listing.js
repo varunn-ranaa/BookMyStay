5a331812e840fdee90db83ab68dd060d6d2cfb2e
varunn_ranaa <varunrana1777@gmail.com>
2026-04-08 16:53:57 +0530

    listing routes

--- routes/listing.js
+++ routes/listing.js
@@ -4,32 +4,24 @@ const Listing = require('../models/listing.js');
 const wrapAsync = require('../utils/wrapAsync.js');
 const ExpressError = require('../utils/expressError.js');
 
-const {LoggedIn, isOwner , listingValidation} = require('../middleware.js');
+const {LoggedIn, isOwner , listingValidation, saveRedirect} = require('../middleware.js');
 const { equal } = require('joi');
 
 const listingController = require('../controllers/listing.js')
 
-
-//All Listings
-router.get("/",wrapAsync(listingController.index));
+router.route("/")
+.get(wrapAsync(listingController.index)) //All Listings
+.post(listingValidation,LoggedIn,wrapAsync(listingController.newListing));//New Listings
 
 //new route
 router.get("/new",LoggedIn,listingController.renderNewForm);
 
-//show route 
-router.get("/:id", wrapAsync(listingController.showListing));
-
-//New Listings
-router.post("/",listingValidation,LoggedIn,wrapAsync(listingController.newListing));
+router.route("/:id")
+.get(wrapAsync(listingController.showListing)) //show route 
+.patch(listingValidation,LoggedIn,isOwner,wrapAsync(listingController.editListing)) //Edit Listings
+.delete(LoggedIn,isOwner,wrapAsync(listingController.deleteListing))//Delete Listings
 
 router.get("/:id/edit",LoggedIn,isOwner,wrapAsync(listingController.editForm));
 
-
-//Edit Listings
-router.patch("/:id",listingValidation,LoggedIn,isOwner,wrapAsync(listingController.editListing));
-
-
-//Delete Listings
-router.delete("/:id",LoggedIn,isOwner,wrapAsync(listingController.deleteListing))
 
 module.exports = router ; 
