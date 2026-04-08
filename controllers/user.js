984be0e95c03ac698e72b249bccbbfc860d94e41
varunn_ranaa <varunrana1777@gmail.com>
2026-04-08 16:55:57 +0530

    redirecting path

--- controllers/user.js
+++ controllers/user.js
@@ -1,42 +1,49 @@
 const User = require('../models/user.js')
 
 module.exports.registerForm = (req, res) => {
-    res.render("users/signUp.ejs", { showNavbar: false });
+    res.render("users/signUp.ejs", { showNavbar: false , returnTo: req.session.returnTo || ""});
 }
 
-module.exports.userRegister = async (req, res) => {
+module.exports.userRegister = async (req, res, next) => {
     try {
         let { username, email, password } = req.body;
         let newUser = new User({ username, email });
-        await User.register(newUser, password)
-        console.log("User Registered !");
-        req.login(newUser, (err) => {
-            if (err) {
-                return next();
-            }
-            req.flash("success", "User Registered successfully !");
-            let redirect = res.locals.redirectUrl || "/listing"
-            res.redirect(redirect);
-        })
+        
+        // Passport-local-mongoose handles hashing and saving
+        const registeredUser = await User.register(newUser, password);
+
+        // Automatically log in the user after registration
+        req.login(registeredUser, (err) => {
+            if (err) return next(err); // Pass the error to the handler
+            
+            req.flash("success", "User Registered successfully!");
+            let redirectUrl = res.locals.redirectUrl || "/listing";
+            res.redirect(redirectUrl);
+        });
+
     } catch (e) {
         req.flash("error", e.message);
-        console.log(e.message);
         res.redirect("/register");
     }
-}
+};
+
 
 module.exports.loginPage = (req, res) => {
-    res.render("users/login.ejs", { showNavbar: false });
+    res.render("users/login.ejs", { showNavbar: false, returnTo: req.session.returnTo || "" });
 }
 
 module.exports.userLogin = async (req, res) => {
     req.flash("success", "User Login Successful !");
-    let redirect = res.locals.redirectUrl || "/listing"
+    let redirect = res.locals.redirectUrl || "/listing";
+   
     res.redirect(redirect);
 
 }
 
 module.exports.userLogout = (req, res, next) => {
+
+    let redirectUrl = req.get('Referer') || "/listings";
+
     req.logout((err) => {
         if (err) {
             return next()
