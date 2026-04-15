d864cd34531bbb3e4145107874de8109e7d5a814
varunn_ranaa <varunrana1777@gmail.com>
2026-04-15 18:37:47 +0530

    listing route

--- routes/listing.js
+++ routes/listing.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const Listing = require('../models/listing.js');
 const wrapAsync = require('../utils/wrapAsync.js');
 const ExpressError = require('../utils/expressError.js');
+const multer  = require('multer')
+const {storage} = require('../cloudConfig.js')
+const upload = multer({ storage })
 
 const {LoggedIn, isOwner , listingValidation, saveRedirect} = require('../middleware.js');
 const { equal } = require('joi');
@@ -11,14 +14,14 @@ const listingController = require('../controllers/listing.js')
 
 router.route("/")
 .get(wrapAsync(listingController.index)) //All Listings
-.post(listingValidation,LoggedIn,wrapAsync(listingController.newListing));//New Listings
+.post(upload.single("listing[image]"),listingValidation,LoggedIn,wrapAsync(listingController.newListing));//New Listings
 
 //new route
 router.get("/new",LoggedIn,listingController.renderNewForm);
 
 router.route("/:id")
 .get(wrapAsync(listingController.showListing)) //show route 
-.patch(listingValidation,LoggedIn,isOwner,wrapAsync(listingController.editListing)) //Edit Listings
+.patch(upload.single("listing[image]"),listingValidation,LoggedIn,isOwner,wrapAsync(listingController.editListing)) //Edit Listings
 .delete(LoggedIn,isOwner,wrapAsync(listingController.deleteListing))//Delete Listings
 
 router.get("/:id/edit",LoggedIn,isOwner,wrapAsync(listingController.editForm));
