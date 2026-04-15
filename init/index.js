239c37bdbab29e77b6487533741e4ade5a5dbe84
varunn_ranaa <varunrana1777@gmail.com>
2026-04-15 18:38:37 +0530

    data

--- init/index.js
+++ init/index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const Listing = require('../models/listing.js');
 const initData = require('./data.js');
+const { init } = require('../models/user.js');
 
 connect()
 .then(res => console.log("Connection Sucessfull !"))
@@ -11,8 +12,11 @@ async function connect(){
   await mongoose.connect('mongodb://127.0.0.1:27017/BookStay')
 }
 
-const initDB = async function (){s
+const initDB = async function (){
     await Listing.deleteMany({});
+    initData.data = initData.data.map((obj)=>({
+      ...obj, owner : "69c52185e4eb75df7d8b64e5",
+    }))
     await Listing.insertMany(initData.data);
     console.log("Data initallized");
 }
