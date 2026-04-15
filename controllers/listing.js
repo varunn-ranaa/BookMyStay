2513879ac9870472a50eb943ca41c9633a01b0b4
varunn_ranaa <varunrana1777@gmail.com>
2026-04-15 18:37:09 +0530

    listing logic

--- controllers/listing.js
+++ controllers/listing.js
@@ -1,4 +1,3 @@
-const { urlencoded } = require('express');
 const Listing = require('../models/listing.js');
 const ExpressError = require('../utils/expressError.js');
 
@@ -13,57 +12,51 @@ module.exports.renderNewForm = (req, res) => {
 
 module.exports.showListing = async (req, res) => {
   let { id } = req.params;
-  let list = await Listing.findById(id).populate({ path: "review", populate: { path: "owner" } }).populate("owner");
+  let list = await Listing.findById(id)
+    .populate({ path: "review", populate: { path: "owner" } })
+    .populate("owner");
 
   if (!list) {
-    req.flash("error", " Listing does not found !");
+    req.flash("error", "Listing does not found !");
     return res.redirect("/listing");
   }
 
-  let lat = 30.3165;  //Geocoding
-  let lng = 78.0322;
-
-  try{
-    let query = encodeURIComponent(`${list.location}, ${list.country}`);
-
-  const geoRes = await fetch(
-      `https://api.geoapify.com/v1/geocode/search?text=${query}&apiKey=${process.env.GEOAPIFY_API_KEY}`
-    );
-   
-  const geoData = await geoRes.json();
-
-  if (geoData.features && geoData.features.length > 0) {
-      lat = geoData.features[0].geometry.coordinates[1]; // latitude
-      lng = geoData.features[0].geometry.coordinates[0]; // longitude
-    }
-  }catch(err){
-  console.log("Geocoding failed:", err.message);
-  }
-
+  const lat = list.map.coordinates[1];
+  const lng = list.map.coordinates[0];
 
   res.render("listings/show.ejs", {
-    list, apiKey: process.env.GEOAPIFY_API_KEY,
+    list,
+    apiKey: process.env.GEOAPIFY_API_KEY,
     lat,
     lng
   });
-}
+};
 
 module.exports.newListing = async (req, res) => {
-
   let data = req.body.listing;
 
-  let file = req.file;
+  // Geocoding API call
+  const query = encodeURIComponent(`${data.location}, ${data.country}`);
+  const geoRes = await fetch(
+    `https://api.geoapify.com/v1/geocode/search?text=${query}&apiKey=${process.env.GEOAPIFY_API_KEY}`
+  );
+  const geoData = await geoRes.json();
 
   let newList = new Listing(data);
   newList.owner = req.user._id;
   newList.image.url = req.file.path;
   newList.image.filename = req.file.filename;
-  console.log(newList);
+
+
+  if (geoData.features && geoData.features.length > 0) {
+    newList.map = geoData.features[0].geometry;
+    //{ type: "Point", coordinates: [lng, lat] }
+  }
 
   await newList.save();
-  req.flash("success", " New Listing Added !");
+  req.flash("success", "New Listing Added !");
   res.redirect("/listing");
-}
+};
 
 module.exports.editForm = async (req, res) => {
   let { id } = req.params;
@@ -81,21 +74,46 @@ module.exports.editListing = async (req, res) => {
     req.flash("error", "Listing not exist !");
     return res.redirect("/listing");
   }
+
   let updatedData = req.body.listing;
 
+
+  console.log(req.file);
+
+  if (req.file) {
+    updatedData.image = {
+      url: req.file.path,
+      filename: req.file.filename
+    };
+  }
+
+  // Geocoding
+  try {
+    const query = encodeURIComponent(`${updatedData.location}, ${updatedData.country}`);
+    const geoRes = await fetch(
+      `https://api.geoapify.com/v1/geocode/search?text=${query}&apiKey=${process.env.GEOAPIFY_API_KEY}`
+    );
+    const geoData = await geoRes.json();
+    if (geoData.features && geoData.features.length > 0) {
+      updatedData.map = geoData.features[0].geometry;
+    }
+  } catch (err) {
+    console.log("Geocoding failed:", err.message);
+  }
+
   let list = await Listing.findByIdAndUpdate(id, updatedData, {
-    runValidators: true, // to donot bypass the validation
-    new: true // retuns updated doc in response
+    runValidators: true,
+    returnDocument: 'after'
   });
-  console.log(list);
+
   if (!list) {
     req.flash("error", "Listing not found!");
     return res.redirect("/listing");
   }
-  //  res.json(list);
+
   req.flash("success", "Listing Edited !");
   res.redirect(`/listing/${id}`);
-}
+};
 
 module.exports.deleteListing = async (req, res) => {
   let { id } = req.params;
